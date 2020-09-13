c2d6a2f7fcb3a0b09b3ac64544afacbf5ecd906d
NicolasRios <nicolasrios91@hotmail.com>
2020-09-12 21:00:25 -0300

    push de guardado

---
+++ MiCalculadora/MiCalculadora/Micalculadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Entidades;
+
+
+namespace MiCalculadora
+{
+    static class Micalculadora
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormCalculadora());
+        }
+    }
+}
