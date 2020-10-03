e5202bcc79502e91afd6c75e794796c2c283bf04
NicolasRios <nicolasrios91@hotmail.com>
2020-10-02 21:15:50 -0300

    Faltan los summary

---
+++ TP-02/Entidades/Ciclomotor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis,marca,color)
+        {
+        }
+
+        /// <summary>
+        /// Las motos son chicas
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            string c = (string)this;
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(c);
+            sb.AppendLine(string.Format("TAMAÑO : {0}", this.Tamanio));
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP-02/Entidades/Suv.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        /// <summary>
+        /// Las camionetas son grandes
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        public sealed override string Mostrar()
+        {
+            string v = (string)this;//me devuelve los datos de la clase base, con el formato del stringBuilder
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("SUV");
+            sb.AppendLine(v);
+
+            sb.AppendLine(string.Format("TAMAÑO : {0}", this.Tamanio));
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
